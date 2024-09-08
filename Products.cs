dc1237c00bff565039a40e4c3bee08b5f85b7034
mirvarimuradova <mirvarimuradova7@gmail.com>
2024-09-09 01:57:51 +0400

    Add project files.

---
+++ Products.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace productswidthobject
+{
+    internal class Products
+    {
+
+      
+        string name, productmodel;
+        decimal productquantity, productprice, totalprice;
+        decimal totalpricepc = 0M, totalpricemobile = 0M;
+        decimal  totalquantity, totalquantitypc =0, totalquantitymobile =0;
+        ArrayList productspc,productsmobile, allproduct;
+
+        public Products()
+        {
+
+            productspc = new ArrayList();
+            productsmobile = new ArrayList();
+            allproduct = new ArrayList();
+            totalprice = 0M;
+            totalquantity = 0;
+
+        }
+
+        //1.Pc , 2.Mobile
+
+        public void Add(string brand , string model, decimal price, int quantity, string category )
+        {
+
+            ArrayList product =new ArrayList();
+
+            product.Add(brand);
+            product.Add(model);
+            product.Add(price);
+            product.Add(quantity);
+            product.Add(category);
+
+            category = category.ToLower();
+            if (category == "pc")
+            {
+                productspc.Add(product);
+                totalpricepc += price*quantity;
+                totalquantitypc += quantity;
+            }
+            else if (category == "mobile")
+            {
+                productsmobile.Add(product);
+                totalquantitymobile += quantity;
+                totalpricemobile += price * quantity;
+            }
+
+            totalprice += totalpricepc + totalpricemobile ;
+            totalquantity += totalquantitymobile + totalquantitypc;
+            allproduct.Add(productspc);
+            allproduct.Add(productsmobile);
+        }
+
+        public void  Showproducts()
+        {
+            int counter = 1;
+            foreach(ArrayList arrayList in allproduct)
+            {
+                Console.WriteLine($"Product: {counter}");
+                foreach(var items in  arrayList)
+                {
+                    Console.WriteLine(items.GetType());
+                    Console.WriteLine(items);
+                }
+                counter++;
+            }
+          
+
+            }
+
+
+        public ArrayList GetAllProducts() {
+
+
+            return allproduct;
+                }
+        public decimal  ShowTotalPrices() {
+
+            return totalprice; 
+
+        }
+
+
+        public decimal TotalpriceBycategory(string category) {
+
+            if (category == "1")
+            {
+                return totalpricepc;
+            }
+            else if (category == "2")
+            {
+
+                return totalpricemobile;
+            }
+            else
+            {
+                return 0M;
+            }
+        
+        }
+        public void ShowbyCategory(string category)
+        {
+
+
+            if (category == "1")
+            {
+                foreach (var items in productspc)
+                {
+                    foreach (var model in (ArrayList)items)
+                    {
+                        Console.WriteLine(model);
+                    }
+
+                }
+                Console.ReadLine();
+            }
+            else if (category == "2") { 
+                foreach(var items in productsmobile)
+                {
+                    foreach(var item in (ArrayList)items)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+                Console.ReadLine();
+            }
+
+        }
+
+        public decimal Totalquantity()
+        {
+            return totalquantity;
+        }
+
+        public decimal Totalquantitipc()
+        {
+            return totalquantitypc;
+        }
+        public decimal Totalquantitimobile()
+        {
+            return totalquantitymobile;
+        }
+
+
+        public void Sell( int count)
+        {
+            if (count > 0 && count <= allproduct.Count)
+            {
+                ArrayList selectedProduct = (ArrayList)allproduct[count - 1];
+
+               
+                decimal currentQuantity = (decimal)selectedProduct[3]; 
+                if (currentQuantity > 0)
+                {
+                    selectedProduct[3] = currentQuantity - 1; 
+                    Console.WriteLine("Product sold successfully.");
+                    Console.ReadLine ();
+                }
+                else
+                {
+                    Console.WriteLine("Product is out of stock.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid product number. Please enter a valid number.");
+            }
+        }
+    }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,227 @@
+﻿using System.Collections;
+using System.Diagnostics.Metrics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace productswidthobject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Products product = new Products();
+
+        restart:
+            Console.Clear();
+            Console.WriteLine("Choose the section:\n" +
+                "1.Add product\n" +
+                "2.Show all product\n" +
+                "3.Total price \n" +
+                "4.Total price by category\n"+
+                "5.Show products by category\n" +
+                "6. Total quantity\n" +
+                "7.Total quantity by category \n" +
+                "8.Sell product");
+
+
+
+            string temp = Console.ReadLine();
+
+
+
+            if (temp == "1")
+            {
+
+                Console.Write("Brand:");
+                string brand = Console.ReadLine();
+
+
+                Console.Write("Model:");
+                string model = Console.ReadLine();
+
+                Console.Write("price:");
+                decimal price = decimal.Parse(Console.ReadLine());
+
+                Console.Write("Quantity:");
+                int quantity = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Category:");
+                string category = Console.ReadLine();
+
+
+                product.Add(brand, model, price, quantity, category);
+
+
+                Console.WriteLine("Do you want to continue?y/n");
+
+                string restart = Console.ReadLine();
+
+                if (restart == "y")
+                {
+                    goto restart;
+                }
+                else if (restart == "n")
+                {
+                    Console.WriteLine("The process is finished!");
+                }
+            }
+            else if (temp == "2")
+            {
+                product.Showproducts();
+
+                Console.WriteLine("Do you want to continue?y/n");
+
+                string restart = Console.ReadLine();
+
+                if (restart == "y")
+                {
+                    goto restart;
+                }
+                else if (restart == "n")
+                {
+                    Console.WriteLine("The process is finished!");
+                }
+            }
+            else if (temp == "3")
+            {
+
+                Console.WriteLine(product.ShowTotalPrices());
+                Console.WriteLine("Do you want to continue?y/n");
+
+                string restart = Console.ReadLine();
+
+                if (restart == "y")
+                {
+                    goto restart;
+                }
+                else if (restart == "n")
+                {
+                    Console.WriteLine("The process is finished!");
+                }
+            }
+
+            else if (temp == "4")
+            {
+
+                Console.WriteLine("Choose the category\n" +
+                    "1.pc\n" +
+                    "2.mobile");
+
+                string category = Console.ReadLine();
+                Console.WriteLine(product.TotalpriceBycategory(category));
+
+
+                Console.WriteLine("Do you want to continue?y/n");
+
+                string restart = Console.ReadLine();
+
+                if (restart == "y")
+                {
+                    goto restart;
+                }
+                else if (restart == "n")
+                {
+                    Console.WriteLine("The process is finished!");
+                }
+            }
+
+
+            else if (temp == "5")
+            {
+
+                Console.WriteLine("Pleace choose the category:\n" +
+                    "1.PC\n" +
+                    "2.Mobile");
+
+                string categorysection = Console.ReadLine();
+                product.ShowbyCategory(categorysection);
+
+
+                Console.WriteLine("Do you want to continue?y/n");
+
+                string restart = Console.ReadLine();
+
+                if (restart == "y")
+                {
+                    goto restart;
+                }
+                else if (restart == "n")
+                {
+                    Console.WriteLine("The process is finished!");
+                }
+
+            }
+
+            else if (temp == "6") {
+
+
+                Console.WriteLine(product.Totalquantity());
+
+                Console.WriteLine("Do you want to continue?y/n");
+
+                string restart = Console.ReadLine();
+
+                if (restart == "y")
+                {
+                    goto restart;
+                }
+                else if (restart == "n")
+                {
+                    Console.WriteLine("The process is finished!");
+                }
+            }
+            else if(temp == "7")
+            {
+                Console.WriteLine(" Which total quantity of category do you want to see?\n" +
+                    "1.pc\n" +
+                    "2.mobile");
+
+                string category= Console.ReadLine();
+
+                if (category == "1")
+                {
+                    Console.WriteLine(product.Totalquantitipc());
+                }
+                else if (category == "2") {
+                    Console.WriteLine(product.Totalquantitimobile());
+                }
+                else
+                {
+                    Console.WriteLine("Wrong value!");
+
+                    Console.WriteLine("Do you want to continue?y/n");
+
+                    string restart = Console.ReadLine();
+
+                    if (restart == "y")
+                    {
+                        goto restart;
+                    }
+                    else if (restart == "n")
+                    {
+                        Console.WriteLine("The process is finished!");
+                    }
+                }
+            }
+
+
+            else if(temp == "8")
+            {
+                int countproduct = product.GetAllProducts().Count;
+                Console.WriteLine("Which product do you want to change, please enter number ?");
+                int number = Convert.ToInt32(Console.ReadLine());
+                if (number > 0 && number <= countproduct)
+                {
+                    product.Sell(number); 
+
+                }
+                else
+                {
+                    Console.WriteLine("No products to sell.");
+                }
+
+            }
+
+        }
+    }
+}
